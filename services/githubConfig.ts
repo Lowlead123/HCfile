1a32555699632ab26f89ca4d442a9b5871cf8f97
Lowlead123 <anypeat@gmail.com>
2025-11-29 20:24:20 +0700

    Update githubConfig.ts
    Signed-off-by: Lowlead123 <anypeat@gmail.com>

--- services/githubConfig.ts
+++ services/githubConfig.ts
@@ -1,6 +1,6 @@
 
 export const GITHUB_CONFIG = {
-    TOKEN: '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',
+    TOKEN: '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',
     OWNER: 'Lowlead123',
     REPO: 'HCFile',
     BRANCH: 'main'
